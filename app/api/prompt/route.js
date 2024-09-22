d597e267f9fc06c34faf2a66f9dd7f2e19fd3ca9
Navin Kumaran <navinkumaran2004@gmail.com>
2024-09-22 18:41:49 +0530

    updated fetch method in feed

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -4,9 +4,7 @@ import { connectToDB } from "@utils/database";
 export const GET = async (request) => {
   try {
     await connectToDB();
-    const prompts = await Prompt.find({})
-      .populate("creator")
-      .sort({ createdAt: -1 });
+    const prompts = await Prompt.find().sort({ createdAt: -1 });
     return new Response(JSON.stringify(prompts), {
       status: 200,
     });
